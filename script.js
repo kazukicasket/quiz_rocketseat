9cbf601d78adb7b44ef1f9640214e8fdf9495993
kiezan <pierresimoncc@gmail.com>
2024-05-17 23:42:57 -0300

    show_answer

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function toggleMode() {
-    const html = document.documentElement
+const html = document.documentElement
+const img = document.querySelector("header img")
+const question = document.querySelector("main p")
+const answer_1 = document.querySelector("#answer-1")
+const answer_2 = document.querySelector("#answer-2")
+const answer_3 = document.querySelector("#answer-3")
+const answer_4 = document.querySelector("#answer-4")
+
+function showAnswerSun() {
+  if (html.classList.contains("light")) {
+    answer_2.className = 'answer'
+  }
+}
+
+function showAnswerMoon() {
+  if (!html.classList.contains("light")) {
+    answer_4.className = 'answer'
+  }
+}
+
+function toggleMode() {    
     html.classList.toggle("light")
-  
-    const img = document.querySelector("header img")
-    const question = document.querySelector("main p")
-    const answer_1 = document.querySelector("#answer-1")
-    const answer_2 = document.querySelector("#answer-2")
-    const answer_3 = document.querySelector("#answer-3")
-    const answer_4 = document.querySelector("#answer-4")
+    answer_2.className = 'notAnswer'
+    answer_4.className = 'notAnswer'
   
     if (html.classList.contains("light")) {
       img.setAttribute("src", "img/logo-light.png")
